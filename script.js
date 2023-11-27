9ff1af532a3ef2eb34bb3e065c7621aec3f94d0e
pgattic <pgattic@gmail.com>
2023-11-27 11:14:17 -0700

    Implement sceneElement class

--- script.js
+++ script.js
@@ -6,15 +6,62 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
-let obj = {
-	dist : 150,
-	height : 50,
+class sceneElement {
+	dist = 0;
+	height = 0;
+	color = "";
+
+	constructor(dist = 0, height = 0, color="red") {
+		this.dist = dist;
+		this.height = height;
+		this.color = color;
+	}
+
+	get screenX(){
+		return canvas.width/2 - this.dist;
+	}
+
+	get screenY(){
+		return canvas.height/2 - this.height;
+	}
+
+	setPosFromCoords(x, y){
+		this.dist = canvas.width/2 - x;
+		this.height = canvas.height/2 - y;
+	}
+
+	render() {
+		ctx.strokeStyle = "#000";
+		ctx.setLineDash([5, 5]);
+		ctx.beginPath();
+		ctx.moveTo(this.screenX, canvas.height/2);
+		ctx.lineTo(this.screenX, this.screenY);
+		ctx.stroke();
+	
+		ctx.beginPath();
+		ctx.arc(this.screenX, this.screenY, 5, 0, Math.PI*2);
+		ctx.fillStyle = "red";
+		ctx.fill();
+	}
 }
+
+let obj = new sceneElement(150, 50);
+let img = new sceneElement();
 
 let focalDist = 100;
 
+function calculateImgFromObj(distObj, htObj, f) {
+	distImg = -1/(1/f-1/distObj);
+
+	//height/height = dist/dist
+	htImg = distImg/distObj * htObj;
+	return [distImg, htImg];
+}
+
 function drawScene() {
+	ctx.strokeStyle = "#000";
 	ctx.setLineDash([]);
+
 	ctx.beginPath();
 	ctx.moveTo(50, canvas.height/2);
 	ctx.lineTo(canvas.width - 50, canvas.height/2);
@@ -30,6 +77,27 @@ function drawScene() {
 	ctx.stroke();
 }
 
+function drawRays() {
+	ctx.strokeStyle = "#000";
+
+	ctx.beginPath();
+	ctx.moveTo(obj.screenX, obj.screenY);
+	ctx.lineTo(canvas.width/2, obj.screenY);
+	ctx.lineTo(img.screenX, img.screenY);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(obj.screenX, obj.screenY);
+	ctx.lineTo(img.screenX, img.screenY);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(obj.screenX, obj.screenY);
+	ctx.lineTo(canvas.width/2, img.screenY);
+	ctx.lineTo(img.screenX, img.screenY);
+	ctx.stroke();
+}
+
 function drawFocalPoint() {
 	ctx.beginPath();
 	ctx.arc(canvas.width/2 - focalDist, canvas.height/2, 5, 0, Math.PI*2);
@@ -37,78 +105,28 @@ function drawFocalPoint() {
 	ctx.fill();
 }
 
-function drawObj() {
-	ctx.setLineDash([5, 5]);
-	ctx.beginPath();
-	ctx.moveTo(canvas.width/2 - obj.dist, canvas.height/2);
-	ctx.lineTo(canvas.width/2 - obj.dist, canvas.height/2 - obj.height);
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.arc(canvas.width/2 - obj.dist, canvas.height/2 - obj.height, 5, 0, Math.PI*2);
-	ctx.fillStyle = "red";
-	ctx.fill();
-
-}
-
-function drawRays() {
-	let [imgX, imgY] = calculateImgFromObj(obj.dist, obj.height, focalDist);
-	ctx.beginPath();
-	ctx.moveTo(canvas.width/2 - obj.dist, canvas.height/2 - obj.height);
-	ctx.lineTo(canvas.width/2, canvas.height/2 - obj.height);
-	ctx.lineTo(canvas.width/2 - imgX, canvas.height/2 - imgY);
-	ctx.stroke();
-	ctx.beginPath();
-	ctx.moveTo(canvas.width/2 - obj.dist, canvas.height/2 - obj.height);
-	ctx.lineTo(canvas.width/2 - imgX, canvas.height/2 - imgY);
-	ctx.stroke();
-	ctx.beginPath();
-	ctx.moveTo(canvas.width/2 - obj.dist, canvas.height/2 - obj.height);
-	ctx.lineTo(canvas.width/2, canvas.height/2 - imgY);
-	ctx.lineTo(canvas.width/2 - imgX, canvas.height/2 - imgY);
-	ctx.stroke();
-}
-
 function drawAll() {
+	[img.dist, img.height] = calculateImgFromObj(obj.dist, obj.height, focalDist);
 	drawScene();
-	drawFocalPoint();
 	drawRays();
-	drawObj();
-	drawImg();
-}
-
-
-function calculateImgFromObj(distObj, htObj, f) {
-	distImg = -1/(1/f-1/distObj);
-
-	//height/height = dist/dist
-	htImg = distImg/distObj * htObj;
-	return [distImg, htImg];
-}
-
-function drawImg() {
-	let [imgX, imgY] = calculateImgFromObj(obj.dist, obj.height, focalDist);
-	ctx.beginPath();
-	ctx.arc(canvas.width/2 - imgX, canvas.height/2 - imgY, 5, 0, Math.PI*2);
-	ctx.fillStyle = "red";
-	ctx.fill();
+	drawFocalPoint();
+	obj.render();
+	img.render();
 }
 
 drawAll();
 
 canvas.onclick=(e)=> {
-	obj.dist = canvas.width/2 - e.layerX;
-	obj.height = -(e.layerY - canvas.height/2);
+	obj.setPosFromCoords(e.layerX, e.layerY);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawAll();
 }
 
 canvas.onmousemove=(e)=> {
 	if (e.buttons) {
-		obj.dist = canvas.width/2 - e.layerX;
-		obj.height = -(e.layerY - canvas.height/2);
+		obj.setPosFromCoords(e.layerX, e.layerY);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		drawAll();
-		}
+	}
 }
 
